ee3b4bf4358d7c78674f0f2892a5e2aa5e5d7ea9
bidhan24 <bidhansingh154@gmail.com>
2023-02-15 10:15:56 +0545

    ssas

--- router/authRoute.js
+++ router/authRoute.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 const authController = require("../controller/authController");
-const Authenticate = require("../middleWare/authenticate");
 
 router.get("/", (req, res) => {
   res.send("hello guyz this is from router");
@@ -16,10 +15,5 @@ router.patch("/get-otp", authController.getOtp);
 router.patch("/verify-otp", authController.verifyOtp);
 
 router.patch("/forget-password", authController.forgetPasswod);
-
-// router.get("/detail", Authenticate, (req, res) => {
-//   console.log("reached");
-//   res.send({ success: true, user: req.rootUser });
-// });
 
 module.exports = router;
