02aa12c8b0e0a8abe2233bb32ed6822d483e6afa
bidhan24 <bidhansingh154@gmail.com>
2023-02-15 11:17:38 +0545

    sasa

--- app.js
+++ app.js
@@ -11,13 +11,8 @@ app.use(express.json());
 const PORT = process.env.PORT;
 
 const cors = require("cors");
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true,
-  optionSuccessStatus: 200,
-};
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(cookieParser());
 
 app.use("/userProfile", express.static("userProfile"));
